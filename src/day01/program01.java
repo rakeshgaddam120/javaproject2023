73b1b4a41b3d2790a55a60b72350e84c05301236
Rakesh Gaddam <rakesh.gaddam120@gmail.com>
2023-02-07 08:09:55 +0530

    day02
    programs

--- src/day01/program01.java
+++ src/day01/program01.java
@@ -1,10 +1,36 @@
 package day01;
 
 public class program01 {
+	
+	
+	
+	
+	
+public static void fun()
+{
+	int x= 123456789;
+	char y = 'a';
+	boolean a= true;
+	byte b = 127;
+	short c = 1433;
+	long d = 123456789;
+	double a1;
+	a1= 123432123445667d;
+	float a2= 1234567.6544325677f;
+	System.out.println(a2);
+	System.out.println(a1);
+	System.out.println(d);
+	System.out.println("good morning");
+	System.out.println(x*x);
+
+				}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("good");
+		
+	
+		fun();
 	}
 
 }
---
+++ src/day02/program02.java
@@ -0,0 +1,24 @@
+package day02;
+
+public class program02 {
+	
+	public static int fun() {
+		int x=10;
+		System.out.println(x*x);
+		return x*x;
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		long a=fun();
+		System.out.println(a+a);
+		
+	}
+
+}
